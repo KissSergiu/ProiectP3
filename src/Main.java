77694e0dc8513b665afba212d82766999f63dbc4
Flavia Kiss <kissflaviamaria@yahoo.com>
2023-12-27 12:28:58 +0200

    Testare commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-/// Testare
+/// Testare commit
 public class Main {
     public static void main(String[] args) {
       /// creatingTableArt("articole");
